69e3d49bfbca95f944247a6621537e1767eb592c
Itzmohsen <156828555+Itzmohsen@users.noreply.github.com>
2024-04-05 21:07:56 -0400

    Create theme-toggle.js

---
+++ static/js/theme-toggle.js
@@ -0,0 +1,25 @@
+// Add this script to your theme-toggle.js file or within a script tag in base.html
+const themeToggle = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    
+    if (currentTheme === 'dark') {
+        themeToggle.textContent = 'Switch to Light Mode';
+    }
+}
+
+themeToggle.addEventListener('click', function () {
+    let theme = document.documentElement.getAttribute('data-theme');
+    
+    if (theme === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        themeToggle.textContent = 'Switch to Dark Mode';
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        themeToggle.textContent = 'Switch to Light Mode';
+    }
+});
